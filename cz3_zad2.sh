c11de62708f50849baa3086b950d4e9868031259
KrzysztofZ123 <97828344+KrzysztofZ123@users.noreply.github.com>
2023-02-08 18:01:44 +0100

    Add files via upload

---
+++ cz3_zad2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Skrypt działa tylko z pełnymi ścieżkami!
+# skróty typu ~/ nie działają
+
+echo "Podaj ścieżkę do wyszukiwania:"
+read fraza
+
+if [ -d $sciezka ]; then
+        echo "Podano ścieżkę do folderu."
+        echo "przeszukiwanie folderu w poszukiwaniu"
+        echo "plików o nazwach zgodnych z podaną frazą:"
+        ls $sciezka | grep $fraza
+
+elif [ -f $sciezka ]; then
+        echo "Podano ścieżkę do pliku."
+        echo "Przeszukiwanie podanego pliku"
+        echo "w celu odnalezienia podanej frazy:"
+        cat $sciezka | grep $fraza
+
+else
+        echo "Błędna ścieżka"
+fi
+
+
